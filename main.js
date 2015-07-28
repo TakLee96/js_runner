916e3be71fbbdaf34388ca81afd473ecc883b4c9
Jiahang Li <jiahang_li@outlook.com>
2015-07-27 17:04:40 -0700

    [Tak] finished the basic skeleton of the project

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 $(document).ready(function () {
 
-    $('#run').on('click', function () {
+    var runButton = $('#run');
+    var inputTextField = $('#input');
+    var outputTextField = $('#output');
+
+    var println = function (text) {
+        outputTextField.val(text);
+    };
+
+    // copied code to handle tab
+    $(document).delegate('#input', 'keydown', function(e) {
+        var keyCode = e.keyCode || e.which;
+
+        if (keyCode == 9) {
+            e.preventDefault();
+            var start = $(this).get(0).selectionStart;
+            var end = $(this).get(0).selectionEnd;
+
+            $(this).val($(this).val().substring(0, start) + "    " + $(this).val().substring(end));
+
+            $(this).get(0).selectionStart = $(this).get(0).selectionEnd = start + 4;
+        }
+    });
+
+    runButton.on('click', function () {
         
+        runButton.html('running');
+        runButton.prop('disabled', true);
+
         $.post('/run', { input: $('#input').val() })
          .success(function (respondObject) {
-             $('#output').val("Program exit with code " + respondObject.code + "\nstdout: " + respondObject.stdout + "\nstderr: " + respondObject.stderr);
+             println("STDOUT:\n" + respondObject.stdout + "\nSTDERR:\n" + respondObject.stderr);
+             runButton.html('run');
+             runButton.prop('disabled', false);
          })
-         .fail(function () {
-             alert("failed!");
+         .fail(function (errorObject) {
+             println("Program has crashed... " + JSON.stringify(errorObject));
+             runButton.html('run');
+             runButton.prop('disabled', false);
          });
 
     });
--- server.js
+++ server.js
@@ -1,39 +1,60 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
+var fs = require('fs');
+var uid = require('uid');
 var app = express();
 
-app.use(log);
+app.use(logMiddlerware);
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.post('/run', run);
+app.use(errorHandlerMiddleware);
 
 app.listen(8080, function () {
     console.log("[Server] listening on port " + this.address().port);
 });
 
-function log (request, response, next) {
+function logMiddlerware (request, response, next) {
     console.log("%s -> %s (%s)", request.method, request.url, new Date());
     next();
 }
 
-function escape(string) {
-    return string.replace(/"/g, "\\\"")
-                 .replace(/'/g, "\\\'")
-                 .replace(/\n/g, "\\n");
+function run (request, response, next) {
+    var jsid = uid();
+    fs.writeFile("data/" + jsid + ".js", request.body.input, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            var child = exec("node data/" + jsid + ".js", {
+                timeout: 2000
+            }, function (err, stdout, stderr) {
+                if (err) {
+                    next(err);
+                } else {
+                    response.status(200).json({
+                        stdout: stdout.toString(),
+                        stderr: stderr.toString()
+                    }).end();
+
+                    fs.unlink("data/" + jsid + ".js", function (err) {
+                        if (err) {
+                            console.error(err);
+                        }
+                    });
+                }
+            });
+        }
+    });
 }
 
-function run (request, response) {
-    console.log("received request with body %s", request.body.input);
-    exec('echo -e \"' + escape(request.body.input) + '\" | node', function (error, stdout, stderr) {
-        response.json({
-            error: error.toString();
-            stdout: stdout.toString(),
-            stderr: stderr.toString()
-        });
-
-        response.end();
-    });
+function errorHandlerMiddleware (error, request, response, next) {
+    if (error) {
+        console.error("An error has occured: " + error.toString());
+        response.status(500).json(error).end();
+    } else {
+        console.error("404 request for " + request.url);
+        response.status(404).end("Page not found...");
+    }
 }
